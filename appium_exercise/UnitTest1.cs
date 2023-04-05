b41bc24f097b47d9d5afd2d2c9daf1c10893f841
iochkolovski <poodsfan29@gmail.com>
2023-04-05 17:34:42 +0300

    first committest

---
+++ appium_exercise/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace appium_exercise
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
